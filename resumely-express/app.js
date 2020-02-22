b8f63ddaaeb2802a01e46070d565cfcba58e99a4
Yassine Ben Hamida <yacinbenhamida@hotmail.fr>
2020-02-22 13:08:12 +0100

    nodemon, dotenv & cors installation
    port set to :5000

--- resumely-express/app.js
+++ resumely-express/app.js
@@ -3,24 +3,36 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var cors = require('cors');
+var dotenv = require('dotenv');
+var mongoose = require('mongoose');
+// routes
+var indexR = require('./routes/index');
+var usersR = require('./routes/users');
 
 var app = express();
+dotenv.config();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 app.use(logger('dev'));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+const uri = process.env.DB_URI;
+mongoose.connect(uri,{useNewUrlParser : true,useCreateIndex : true});
+const connection = mongoose.connection;
+connection.once('open',()=>{
+  console.log('Resumely connected to remote atlas DB');
+});
+
+app.use('/', indexR);
+app.use('/users', usersR);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
