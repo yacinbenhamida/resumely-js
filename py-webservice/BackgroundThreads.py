a4a421d774847a738fb23c5533d1ff28fb431bbe
Yassine Ben Hamida <yacinbenhamida@hotmail.fr>
2020-05-07 00:13:16 +0100

    path problems

--- py-webservice/BackgroundThreads.py
+++ py-webservice/BackgroundThreads.py
@@ -1,11 +1,17 @@
 from flask import Flask, jsonify, request, Response
 from threading import Thread
 from flask_restful import Resource, Api, reqparse
-from customscrapping import scrapdata
+import sys
+from pathlib import Path
 from flask_jwt_extended import (
     JWTManager, jwt_required, create_access_token,
     get_jwt_identity
 )
+BASE_DIR = Path(__file__).resolve().parent
+SCRAP_DIR = BASE_DIR / 'customscrapping'
+sys.path.append(str(SCRAP_DIR))
+from customscrapping import scrapdata
+
 class BackgroundThread(Resource):  
     @jwt_required   
     def get(self,country,idop):
