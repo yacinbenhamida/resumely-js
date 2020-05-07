35290932083c7fa471fdbf012265578fdef7ba06
Yassine Ben Hamida <yacinbenhamida@hotmail.fr>
2020-05-07 22:44:23 +0100

    Update connection.js

--- resumely-express/elasticsearch/connection.js
+++ resumely-express/elasticsearch/connection.js
@@ -1,7 +1,9 @@
 const es = require('elasticsearch');
 const esClient = new es.Client({
     host: 'http://51.178.142.162:9200/',
-    log: 'trace'
+    log: 'trace',
+    requestTimeout: Infinity, // Test
+    keepAlive: true // Test
 });
 
 module.exports = esClient;
