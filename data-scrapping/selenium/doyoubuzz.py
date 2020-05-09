cb8dea62c9e35a8c9e6e916d6b78078a2c9470bb
Yassine Ben Hamida <yacinbenhamida@hotmail.fr>
2020-05-09 16:10:16 +0100

    Update doyoubuzz.py

--- data-scrapping/selenium/doyoubuzz.py
+++ data-scrapping/selenium/doyoubuzz.py
@@ -35,9 +35,16 @@ extracted_data = {}
 extracted_data['candidates'] = []
 options = Options()
 options.set_headless(headless=True)
-#options.add_argument('--headless')
-#options.add_argument('--no-sandbox')
-#options.add_argument('--disable-dev-shm-usage')
+options.add_argument("--window-size=1920,1080")
+options.add_argument("--disable-extensions")
+options.add_argument("--proxy-server='direct://'")
+options.add_argument("--proxy-bypass-list=*")
+options.add_argument("--start-maximized")
+options.add_argument('--headless')
+options.add_argument('--disable-gpu')
+options.add_argument('--disable-dev-shm-usage')
+options.add_argument('--no-sandbox')
+options.add_argument('--ignore-certificate-errors')
 options.add_argument("--lang=fr");
 service_log_path = '/tmp/local/chromedriver.log'
 
@@ -58,11 +65,6 @@ sleep(4)
 
 pages=driver.find_elements_by_xpath("//*[@class='AaVjTc']/tbody/tr/td/a")
 print(pages)
-
-test = driver.find_element_by_xpath("//*[starts-with(@class,'gLFyf')]");
-print(str(test))
-test1 = driver.find_element_by_xpath("//*[starts-with(@id,'result-stats')]");
-print(str(test1))
 youbuzz_urls = []
 try:
     if driver.find_element_by_xpath("//*[contains(text(), 'Suivant')]") :
